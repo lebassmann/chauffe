1da4cdf02069155a40bcaa118c8e65cd29afd01a
arez <jesuisdev1906moi@gmail.com>
2017-08-11 16:58:58 +0200

    Chauffe project commit

---
+++ NetBeans/Chauffe/src/java/marcel/marcel.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package marcel;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author jazz
+ */
+@WebService(serviceName = "marcel")
+public class marcel {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
