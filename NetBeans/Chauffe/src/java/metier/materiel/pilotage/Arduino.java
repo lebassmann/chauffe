ed69e3b6d09c773b5cc2363dc379f5868a5a3b4c
arez <jesuisdev1906moi@gmail.com>
2017-08-15 09:19:06 +0200

    Delete unused source package

---
+++ NetBeans/Chauffe/src/java/metier/materiel/pilotage/Arduino.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metier.materiel.pilotage;
+
+/**
+ *
+ * @author jazz
+ */
+public abstract class  Arduino implements IArduino{
+
+    int arduinoID;
+    String arduinoBluetoothID;
+    /**
+     * Set an ID for the Arduino card, must be in range of 0-255
+     * This depending of the protocole implementation taht accepte only
+     * 256 cards in system
+     * @param arduinoId An integer beetwen 0-255
+     */
+    @Override
+    public void setArduinoID(int arduinoId) {
+        this.arduinoID=arduinoId;
+    }
+
+    @Override
+    public void setArduinoBlutoothID(String arduinoBluetoothID) {
+        this.arduinoBluetoothID = arduinoBluetoothID;
+    }
+    
+}
---
+++ NetBeans/Chauffe/src/java/metier/materiel/pilotage/ArduinoPilote.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metier.materiel.pilotage;
+
+/**
+ *
+ * @author jazz
+ */
+public class ArduinoPilote extends Arduino{
+
+    @Override
+    public void setArduinoBlutoothID(String arduinoBluetoothID) {
+        super.setArduinoBlutoothID(arduinoBluetoothID); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setArduinoID(int arduinoId) {
+        super.setArduinoID(arduinoId); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
